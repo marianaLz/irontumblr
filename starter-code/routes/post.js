f5e3a8ce91a554efce5d2e44d215332d75ca4b3f
Kary Mora <53192777+karymora@users.noreply.github.com>
2019-09-15 18:34:35 -0500

    Update post.js

--- starter-code/routes/post.js
+++ starter-code/routes/post.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const router = express.Router();
-const Post = require('../models/Post');
-const uploadCloud =require('../config/cloudinary')
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/Post')
+const uploadCloud = require('../config/cloudinary')
 
 const isLoggedIn = (req, res, next) => {
   if (req.session.currentUser) {
     next()
   } else {
-    res.redirect("/");
+    res.redirect('/auth/signup')
   }
-};
+}
 
 router.get('/createpost', isLoggedIn, (req, res) => {
   res.render('post-add')
 })
 
-router.post('/post/add', uploadCloud.single('photo'),async(req,res) => {
-  let {content} = req.body
+router.post('/post/add', uploadCloud.single('photo'), async (req, res) => {
+  let { content } = req.body
   let picName = req.file.originalname
-  let  picPath=req.file.url
-  await Post.create({content,picName,picPath})
+  let picPath = req.file.url
+  await Post.create({ content, picName, picPath })
   res.redirect('/')
 })
 
-
-module.exports = router;
+module.exports = router
