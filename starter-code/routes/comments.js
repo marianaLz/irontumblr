76a86077031809523777a3fa039c4839c7c9c4a3
Kary Mora <53192777+karymora@users.noreply.github.com>
2019-09-15 18:33:28 -0500

    Update comments.js

--- starter-code/routes/comments.js
+++ starter-code/routes/comments.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const router = express.Router();
-const Comment = require('../models/Comment');
-const uploadCloud =require('../config/cloudinary')
+const express = require('express')
+const router = express.Router()
+const Comment = require('../models/Comment')
+const uploadCloud = require('../config/cloudinary')
 
 const isLoggedIn = (req, res, next) => {
   if (req.session.currentUser) {
     next()
   } else {
-    res.redirect("/");
+    res.redirect('/auth/signup')
   }
-};
+}
 
 router.get('/createcomment', isLoggedIn, (req, res) => {
   res.render('create-comment')
 })
 
-router.post('/createcomment', uploadCloud.single('photo'), async(req,res) => {
-  let {content} = req.body
+router.post('/createcomment', uploadCloud.single('photo'), async (req, res) => {
+  let { content } = req.body
   let comImgName = req.file.originalname
-  let  comImgPath=req.file.url
-  await Comment.create({content,comImgName,comImgPath})
+  let comImgPath = req.file.url
+  await Comment.create({ content, comImgName, comImgPath })
   res.redirect('/')
 })
 
-
-module.exports = router;
+module.exports = router
