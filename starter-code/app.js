7c82dacd6218ff375623f4f198cb770b3e729b55
Imanol Sanchez <you@example.com>
2019-09-16 09:32:09 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,43 +1,40 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const favicon = require('serve-favicon')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const path = require('path')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 mongoose
-  .connect(process.env.DB || "mongodb://localhost/lab-file-upload", { useNewUrlParser: true })
+  .connect(process.env.DB, { useNewUrlParser: true })
   .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
+    console.error('Error connecting to mongo', err)
+  })
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
-);
+const app_name = require('./package.json').name
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
 
-const app = express();
+const app = express()
 
 // Middleware Setup
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use(
   session({
-    secret: "Mariana_15",
+    secret: 'Mariana_15',
     resave: true,
     cookie: { maxAge: 60000 },
     store: new MongoStore({
@@ -45,29 +42,29 @@ app.use(
       ttl: 24 * 60 * 60 // 1 day
     })
   })
-);
+)
 
 // Express View engine setup
 
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
     sourceMap: true
   })
-);
+)
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
+app.locals.title = 'Express - Generated with IronGenerator'
 
-const index = require("./routes/index");
-const auth = require("./routes/auth");
-app.use("/", index);
-app.use("/auth", auth);   
+const index = require('./routes/index')
+const auth = require('./routes/auth')
+app.use('/', index)
+app.use('/auth', auth)
 
-module.exports = app;
+module.exports = app
--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    userImg: {
+      type: String,
+      default: 'https://techinfofeed.com/wp-content/uploads/2019/02/cute-whatsapp-dp-images-40.jpg'
+    }
   },
-  password: {
-    type: String,
-    trim: true,
-    required: true
-  }
-},
-{ timestamps: true }
-);
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
